64565cd60703548853f3d04a4ef2f375cbae627e
Ethan Eldridge <ejayeldridge@gmail.com>
2024-03-14 00:16:06 -0400

    Initial Commit: Run Project Generate command

---
+++ desktop/src/space/peetseater/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package space.peetseater.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import space.peetseater.game.Match3Game;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Match3Tutorial");
+		new Lwjgl3Application(new Match3Game(), config);
+	}
+}
